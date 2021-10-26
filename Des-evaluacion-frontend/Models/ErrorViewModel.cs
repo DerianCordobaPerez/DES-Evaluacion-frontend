0817ef96914d72122b906b0cd10ecfebbc78c1fa
DerianCordobaPerez <derianunanleon@gmail.com>
2021-10-26 13:31:51 -0600

    Modificacion del proyecto entero

---
+++ Des-evaluacion-frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Des_evaluacion_frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
