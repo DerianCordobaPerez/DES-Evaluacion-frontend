6bef8a0d118761fcc16333a882a0ffa9537eae34
DerianCordobaPerez <derianunanleon@gmail.com>
2021-10-27 17:12:41 -0600

    Creacion del modelo para la informacion de los datos

---
+++ Models/CountryInformation.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Des_evaluacion_frontend.Models
+{
+    public class CountryInformation
+    {
+        [JsonPropertyName("COUNTRY")]
+        public string Country { get; set; }
+        
+        [JsonPropertyName("ISO_CODE")]
+        public string IsoCode { get; set; }
+        
+        [JsonPropertyName("data")]
+        public List<DataInformation> Data { get; set; }
+
+        public abstract class DataInformation
+        {
+            [JsonPropertyName("date")]
+            public DateTime Date { get; set; }
+            
+            [JsonPropertyName("total_vaccinations")]
+            public long TotalVaccinations { get; set; }
+            
+            [JsonPropertyName("vaccine")]
+            public string Vaccine { get; set; }
+        }
+        
+    }
+}
