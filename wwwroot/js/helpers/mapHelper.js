7aebbeb0769207e32596e3eed0555a2390a48f98
DerianCordobaPerez <derianunanleon@gmail.com>
2021-10-30 15:10:51 -0600

    Modificacion del grafico de barras dando la informacion correcta y ajustando el tema

--- wwwroot/js/helpers/mapHelper.js
+++ wwwroot/js/helpers/mapHelper.js
@@ -1,7 +1,25 @@
-const addMaker = (map, lat, lng) => {
-    console.log(lat, lng);
+let lastMarker
+
+const addMaker = (lat, lng) => {
+    const marker = new google.maps.Marker({
+        position: { lat, lng}, 
+        map
+    })
     
-    new google.maps.Marker({
-        position: { lat, lng}, map
-    })   
+    const infoWindow = new google.maps.InfoWindow({
+        content: '<h1>Hola mundo</h1>'
+    })
+
+    google.maps.event.addListener(marker, 'click', ((marker, infoWindow) => {
+        return () => {
+            closeLastInfoWindow()
+            infoWindow.open(map, marker)
+            lastMarker = infoWindow
+        }
+    })(marker, infoWindow))
+}
+
+const closeLastInfoWindow = () => {
+    if (lastMarker)
+        lastMarker.close()
 }
