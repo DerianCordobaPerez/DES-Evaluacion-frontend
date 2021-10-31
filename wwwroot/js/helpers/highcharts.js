287e0ef5709af446498ebbbcb5fcd0787b75619a
DerianCordobaPerez <derianunanleon@gmail.com>
2021-10-30 18:35:23 -0600

    Asignacion de la informacion para el grafico de lineas

--- wwwroot/js/helpers/highcharts.js
+++ wwwroot/js/helpers/highcharts.js
@@ -74,10 +74,32 @@ const setThemeHighChart = () => {
 const getExtendedCountryInformation = async () => 
     await getAllInformation("extendedCountryInformation/getAll").then(response => this.data = response)
 
-const getCasesPercentage = () => {
+const getCasesPerPercentage = () => {
     const data = this.data.map(item => item.data)
     const sum = data.map(item => getSumFromKey('totalCases', item))
     return sum.map(item => item / sum.reduce((sum, item) => sum + item, 0) * 100)
+}
+
+const getTotalDeathsPerDays = () => {
+    let index = 0
+    const data = this.data.map(item => item.data)
+    
+    console.log(data)
+    
+    // get first four days
+    const latestDays = data.map(item => item[index++].date).slice(0, 4)
+    
+    // filter the data to get the total deaths per day
+    const deathsPerDays = data.map(item => 
+        item.filter(
+            item => latestDays.includes(item.date))
+        )
+    
+    const totalDeaths = deathsPerDays.map(item => 
+        item.map(item => item.totalDeaths)
+    )
+    
+    return {totalDeaths, latestDays}
 }
 
 /**
@@ -94,15 +116,12 @@ const createHighChartBar = () => {
     }
 
     let index = -1
-    const percentage = getCasesPercentage()
+    const percentage = getCasesPerPercentage()
     
-    const series = this.data.map((item) => {
-        ++index
-        return ({
-            name: item.location,
-            data: [parseFloat(percentage[index].toFixed(2))]
-        })
-    })
+    const series = this.data.map((item) => ({
+        name: item.location,
+        data: [parseFloat(percentage[++index].toFixed(2))]
+    }))
     
     printHighChart(series, properties)
 }
@@ -115,16 +134,18 @@ const createMarker = () => this.data.map(item => addMaker(parseFloat(item.latitu
  * @returns {void} 
  **/
 const createHighChartLine = () => {
+    const {totalDeaths, latestDays} = getTotalDeathsPerDays()
+    
     const properties = {
         name: 'highchart-line',
         type: 'line',
         title: {text: 'Fallecidos por COVID-19'},
-        xAxis: this.data.map(item => item.location),
+        xAxis: latestDays,
     }
     
-    const series = this.data.map(item => ({
+    const series = this.data.map((item, index) => ({
         name: item.location,
-        data: []
+        data: totalDeaths[index]
     }))
 
     printHighChart(series, properties)
