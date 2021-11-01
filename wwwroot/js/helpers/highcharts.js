eeae8260fab85c31100a9e14b9f4b6b571208e50
DerianCordobaPerez <derianunanleon@gmail.com>
2021-10-31 20:09:43 -0600

    Creacion de la informacion de la ventana modal

--- wwwroot/js/helpers/highcharts.js
+++ wwwroot/js/helpers/highcharts.js
@@ -1,72 +1,4 @@
 /**
- * They display the highchart with the data
- * passed as parameters.
- *
- * @param {Object} series
- * @param {Object} properties
- * @returns {void}
- * */
-const printHighChart = (series, properties) => {
-    const {name, type, title, xAxis} = properties
-    
-    Highcharts.chart(`${name}`, {
-        chart: {type},
-        title,
-        xAxis: {categories: xAxis},
-        yAxis: {title: ''},
-        series
-    })
-}
-
-const setThemeHighChart = () => {
-    Highcharts.theme = {
-        colors: ["#017E73", "#E91D63", "#673AB7", "#FDCD36", "#DAE8F0", "#DDE0E3", "#FADEC7"],
-        chart: {
-            backgroundColor: null,
-            style: {
-                fontFamily: "Tahoma, serif"
-            }
-        },
-        title: {
-            style: {
-                color: '#00ACEC',
-                font: 'bold 16px, Tahoma, sans-serif'
-            }
-        },
-        subtitle: {
-            style: {
-                color: '#00ACEC',
-                font: 'bold 16px, Tahoma, sans-serif'
-            }
-        },
-        tooltip: {
-            borderWidth: 0
-        },
-        legend: {
-            itemStyle: {
-                fontWeight: '',
-                fontSize: '11px'
-            }
-        },
-        xAxis: {
-            labels: {
-                style: {
-                    color: '#6e6e70'
-                }
-            }
-        },
-        yAxis: {
-            labels: {
-                style: {
-                    color: '#6e6e70'
-                }
-            }
-        }
-    };
-    Highcharts.setOptions(Highcharts.theme);
-}
-
-/**
  * Obtains the data in an extended way from the countries api.
  * 
  * @return {Promise} 
@@ -80,11 +12,15 @@ const getCasesPerPercentage = () => {
     return sum.map(item => item / sum.reduce((sum, item) => sum + item, 0) * 100)
 }
 
+const getLatestDays = (data) => {
+    
+}
+
 const getTotalDeathsPerDays = () => {
-    let index = 0
     const data = this.data.map(item => item.data)
    
     // get first four days
+    let index = 0
     const latestDays = data.map(item => item[index++].date).slice(0, 4)
     
     // filter the data to get the total deaths per day
--- wwwroot/js/helpers/modalHelper.js
+++ wwwroot/js/helpers/modalHelper.js
@@ -6,13 +6,18 @@ const numberWithCommas = (number) =>
 
 const createModalWindow = async (country) => {
     
-    await getCountryInformation()    
+    await getCountryInformation()
     const results = this.results.filter(item => item.country === country)
     const totalVaccinations = results.map(item => item.data.map(element => element.totalVaccinations))
     const max = Math.max(...totalVaccinations.flat())
+    let vaccines = this.results.map(item => item.data.map(element => element.vaccine.split(',').flat())).flat().flat()
+    vaccines = vaccines.map(item => item.trim())
+    const resultsVaccines = vaccines.filter((item, index) => vaccines.indexOf(item.trim()) === index)
     
-    console.log(results)
-    
+    let percentage = (max / 10000).toFixed(2)
+    if(percentage > 100)
+        percentage /= 10
+
     return `
         <div class="modal">
             <div class="modal-header">
@@ -35,19 +40,57 @@ const createModalWindow = async (country) => {
                 
                 <div class="modal-header-percentage">
                     <div class="modal-multiple-lines-content">
-                        <h3 class="modal-title-span">${numberWithCommas(max)}</h3>
-                        <p class="modal-subtitle-span">Personas vacunadas</p>                    
+                        <h3 class="modal-title-span">${percentage}%</h3>
+                        <p class="modal-subtitle-span">Porcentaje vacunados</p>                    
                     </div>
+                </div>
+                
+                <div class="modal-header-selector">
+                    <select class="modal-selector">
+                        ${resultsVaccines.map(item => `<option value="${item}">${item}</option>`).join('')}
+                    </select>
                 </div>
             </div>
             
             <div class="modal-body">
-                
+                <div id="modal-highchart"></div>
             </div>
         </div>
     `
 }
 
+const printModalHighchart = () => {
+    const days = [
+        "2021-09-01",
+        "2021-09-02",
+        "2021-09-03",
+        "2021-09-04"
+    ]
+
+    const properties = {
+        name: 'modal-highchart',
+        type: 'line',
+        title: {text: ''},
+        xAxis: days,
+    }
+
+    const results = this.results.map(item => item.data)
+
+    const vaccinesPerDay = results.map(item =>
+        item.filter(item => item.date).slice(0, 4))
+
+    const totalVaccines = vaccinesPerDay.map(item =>
+        item.map(item => item.totalVaccinations))
+
+    const series = this.results.map((item, index) => ({
+        name: days[index],
+        data: totalVaccines[index]
+    }))
+
+    printHighChart(series, properties)
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
-    //await getCountryInformation()
+    await getCountryInformation()
+    printModalHighchart()
 })
